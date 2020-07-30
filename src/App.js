79ddbee44e2a90356e7ba3f60a9f714af2af52ef
isbhargav <ladbhargav@yahoo.co.in>
2020-07-29 23:41:18 -0400

    chakra reset

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
-
+import { Button } from "@chakra-ui/core";
 function App() {
   return (
-    <div>Hello</div>
+    <div>
+      <Button size="xs" variantColor="green">Button</Button>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ThemeProvider, CSSReset } from '@chakra-ui/core'
 
 ReactDOM.render(
-  <App />,
+  <ThemeProvider>
+    <CSSReset />
+    <App />
+  </ThemeProvider>
+  ,
   document.getElementById('root')
 );
