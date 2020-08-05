7c4f2c4ca16c0393712b771a9a2e113b263e4adc
isbhargav <ladbhargav@yahoo.co.in>
2020-08-05 19:41:49 -0400

    Updates

---
+++ precache-manifest.b6c92b91ac1d57d65ea7fcd4d2bb80e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cae74f544ae5ab63f7cbca49c728d0a",
+    "url": "/pokemon-card-game/index.html"
+  },
+  {
+    "revision": "ad337d6435a749407400",
+    "url": "/pokemon-card-game/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "e5c6bcaf4246eb811e55",
+    "url": "/pokemon-card-game/static/js/2.d6a45c9a.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/pokemon-card-game/static/js/2.d6a45c9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad337d6435a749407400",
+    "url": "/pokemon-card-game/static/js/main.0e38e00a.chunk.js"
+  },
+  {
+    "revision": "560f8aed814693d03f2b",
+    "url": "/pokemon-card-game/static/js/runtime-main.e2aca6ad.js"
+  }
+]);
